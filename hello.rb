a0dbeb883936f105c151227ce6a8edda94c03586
”酒井” <sakai531932@gmail.com>
2020-10-02 12:56:16 +0000

    ruby02kadai

--- hello.rb
+++ hello.rb
@@ -1,6 +1,29 @@
-p "Branding Engineer"
-p "Hello"
-p "HR Tech"
-puts 1
-puts 2
-puts 3
+puts "初めてのRuby"
+print "Ruby"
+print "始めました"
+puts ""
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+num_array = [1, 2, 3, 4]
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+nobu = { name: 'nobuyuki sakai', birthday: 19921010, bloodtype: "a" }
+def plus_ruby(word)
+  p word + "ruby"
+end
+plus_ruby("like")
+plus_ruby("love")
+
+def plus_one(number)
+p number.next
+end
+plus_one(3)
+plus_one(5)
+
+
+
+
