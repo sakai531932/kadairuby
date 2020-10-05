f86b36b56071947498f061474e9e1752ea998df5
”酒井” <sakai531932@gmail.com>
2020-10-05 12:18:12 +0000

    ruby04kadai2

--- kadai04.rb
+++ kadai04.rb
@@ -1,17 +1,12 @@
 class People
-  def initialize
+ attr_accessor :name
+  def initialize(name)
+    @name = name
     p "Peopleのインスタンスが作られました"
   end
   def self.introduce
     p "私はPeopleクラスです"
   end
-  def name=(value)
-  @name = value
-  end
-  def name
-    @name
-  end
-  attr_accessor :name
 end
 
 class ChildPeople < People
@@ -20,14 +15,11 @@ class ChildPeople < People
   end
 end
 
-people = People.new
 People.introduce
-people1 = People.new
-people1.name = "タカシ"
+people1 = People.new('タカシ')
 p people1.name
-people2 = People.new
-people2.name = "ケント"
-p people2.name
+people1.name = 'ケント'
+p people1.name
 ChildPeople.introduce
 ChildPeople.greet
 
